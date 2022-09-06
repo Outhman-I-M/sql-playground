13941d6cf767ad6e5fdde3cdaa38eb59f3134264
Outhman-I-M <86215135+Outhman-I-M@users.noreply.github.com>
2022-09-06 13:06:59 +0000

    Update csv_sql.py

--- csv_sql.py
+++ csv_sql.py
@@ -1,8 +1,10 @@
 # code inspired from post by George Pearse at https://medium.com/mlearning-ai/how-to-start-learning-sql-with-streamlit-d3edad7494cd
+from operator import index
 import sqlite3
 import streamlit as st
 import pandas as pd
 import os
+
 
 
 def create_connection(db_file):
@@ -50,30 +52,41 @@ def upload_data():
         #read csv
         try:
             df = pd.read_csv(uploaded_file,index_col=0)
-            df.to_sql(name=table_name, con=conn)
-            st.write('Data uploaded successfully. These are the first 5 rows.')
-            st.dataframe(df.head(5))
+            if ValueError:
+                st.write('Table already exists! Do you want to replace or choose another name?')
+                table_replace = st.button('Replace table')
+                table_rename = st.button('Choose another name')
+                if table_rename:
+                    df.to_sql(name=table_name, con=conn, if_exists='fail')
+                    st.write('Data uploaded successfully. These are the first 5 rows.')
+                    st.dataframe(df.head(5))
+                elif table_replace:
+                    df.to_sql(name=table_name, con=conn, if_exists='replace')
+                    st.write('Data uploaded successfully. These are the first 5 rows.')
+                    st.dataframe(df.head(5))                   
 
         except Exception as e:
             st.write(e)
 
-#@st.cache # IMPORTANT: Cache the conversion to prevent computation on every rerun
 def view_table():
     st.markdown("# View Table(s)")
     sqlite_dbs = [file for file in os.listdir('.') if file.endswith('.db')]
     db_filename = st.selectbox('DB Filename', sqlite_dbs)
     conn = create_connection(db_filename)
     table_name = st.selectbox('Table(s) in database', pd.read_sql("select name from sqlite_master where type='table'",con=conn))
-    dataframe = pd.read_sql_query("select * from " + table_name, con=conn)
-    st.write('These are the first 5 rows of the',table_name,'table.')
-    st.dataframe(dataframe.head())
-    download_df = dataframe.to_csv().encode('utf-8')
-    st.download_button(
-     label="Download "+table_name+" table as CSV",
-     data=download_df,
-     file_name=table_name+'.csv',
-     mime='text/csv',
-     )
+    if table_name is None:
+        st.write('No table(s) in database')
+    else:
+        dataframe = pd.read_sql_query("select * from " + table_name, con=conn)
+        st.write('These are the first 5 rows of the',table_name,'table.')
+        st.dataframe(dataframe.head())
+        download_df = dataframe.to_csv().encode('utf-8')
+        st.download_button(
+        label="Download "+table_name+" table as CSV",
+        data=download_df,
+        file_name=table_name+'.csv',
+        mime='text/csv',
+        )
     st.sidebar.markdown("# View Table(s)")
 
 def run_query():
@@ -97,8 +110,25 @@ def run_query():
             st.dataframe(results_df)
         except Exception as e:
             st.write(e)
-
     st.sidebar.markdown("# Run Query")
+
+    # https://discuss.streamlit.io/t/save-user-input-into-a-dataframe-and-access-later/2527/2
+    @st.cache(allow_output_mutation=True) 
+    #@st.experimental_memo # clear persisted dataframe
+    def get_query():
+        return []
+
+    save_query = st.button("Save Query")
+    if save_query:
+        if query == "":
+            st.write("Cannot save empty query")
+        else:
+            get_query().append({"Database":db_filename,"Query":query})
+
+    view_query = st.button("View Saved Queries")
+    if view_query:
+        dataframe = pd.DataFrame(get_query()).drop_duplicates(["Query"])
+        st.table(dataframe)
 
 page_names_to_funcs = {
     "Create Database": create_database,
